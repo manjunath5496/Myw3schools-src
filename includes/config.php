dc769e90b335a99b83ab87feebf962a8caebc7f6
Manjunath.R <manjunath5496@gmail.com>
2021-03-11 08:09:37 -0800

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,44 @@
+<?php
+ob_start();
+
+
+//database credentials
+define('DBHOST','localhost');
+define('DBUSER','mywschoo_user');
+define('DBPASS','maczen5496');
+define('DBNAME','mywschoo_db');
+
+$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//set timezone
+date_default_timezone_set('Europe/London');
+
+//load classes as needed
+function __autoload($class) {
+   
+   $class = strtolower($class);
+
+	//if call from within assets adjust the path
+   $classpath = 'classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	} 	
+	
+	//if call from within admin adjust the path
+   $classpath = '../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	}
+	
+	//if call from within admin adjust the path
+   $classpath = '../../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	} 		
+	 
+}
+
+$user = new User($db); 
+?>
